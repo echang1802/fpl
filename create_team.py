69c2ceeb84bbbe9676078347f4640f0700856999
Eloy Chang <eloy.chang.182@gmail.com>
2021-03-24 10:16:40 -0300

    Created file to start the team

--- create_team.py
+++ create_team.py
@@ -5,7 +5,7 @@ import pandas as pd
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 r = requests.get(url)
 json = r.json()
-elements = pd.DataFrame(json['elements'])[["element_type", "first_name", "second_name", "id", "team", "value_season", "total_points", "web_name"]].set_index("id")
+elements = pd.DataFrame(json['elements'])[["code", "element_type", "first_name", "second_name", "team", "value_season", "total_points"]]
 elements["value_season"] = elements.value_season.astype(float)
 elements["total_points"] = elements.total_points.astype(int)
 elements["cost"] = elements.total_points / elements.value_season
@@ -17,7 +17,11 @@ elements.drop(columns = "element_type", inplace = True)
 teams = pd.DataFrame(json['teams'])[["id", "name"]].set_index("id")
 elements["team"] = elements.team.map(teams.name)
 
-team_players = ["Martínez", "Johnstone", "Mings", "Dallas", "Justin", "Stones", "Bednarek", "Grealish", "Salah", "Fernandes", "Son", "Soucek", "Bamford", "Brewster", "Kane"]
+team_players = [98980, 218023, 177815, 153366, 87873, 118748, 59859, 97299, 171314, 141746, 195473, 78830, 85971, 101982, 55459]
+bank = 0.1
 
-team = elements.loc[elements.web_name.isin(team_players)]
-print(team.head(15))
+team = elements.loc[elements.code.isin(team_players)]
+print(team)
+
+with open("data/mokaFC", "wb") as file:
+    pickle.dump((team, bank), file)
