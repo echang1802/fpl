758202e804835c57f4b1d99999b1324e32263d99
echang1802 <41762304+echang1802@users.noreply.github.com>
2021-04-23 09:52:52 -0300

    Dashboard data creation class ready to test

--- dashboard_data_class.py
+++ dashboard_data_class.py
@@ -4,6 +4,7 @@ import pandas as pd
 class dashboard_data:
 
     def __init__(self, data_type, fpl_class):
+        self._data_type = data_type
         if data_type == "gameweek":
             self._get_gameweek_data(fpl_class)
         elif data_type == "season":
@@ -37,3 +38,51 @@ class dashboard_data:
         self.data["position"] = self.data.element_type.map(fpl_class.element_types.singular_name)
         self.data["team"] = self.data.team.map(fpl_class.clubs.name)
         self.data.drop(columns = ["element_type"], inplace = True)
+
+    def _get_top_players_by_postition(self, position):
+        topPlayers = self.data.loc[self.data.position == position]
+        topPlayers["cost"] *= -1
+        return topPlayers.sort_values(by = ["event_points", "cost"], ascending = False).head(10).reset_index(drop = True)
+
+    def _validate_team(self, dreamteam):
+        if dreamteam.shape[0] < 11:
+            return False
+        min_position_limit = {
+            "Defender" : 3,
+            "Midfielder" : 2,
+            "Forward" : 1
+        }
+        for position, limit in min_position_limit.items():
+            if (dreamteam.position == position).sum() < limit:
+                lower_value_player = dreamteam.loc[dreamteam.position != position].sort_values(by = ["event_points", "cost"]).index[0]
+                dreamteam.drop(lower_value_player, inplace = True)
+                return False
+        return True
+
+    def generate_gameweek_dreamteam(self):
+        # Goalkeeper
+        dreamteam = self._get_top_players_by_postition("Goalkeeper").head(1)
+
+        # Best minimal quantity of player by each position
+        positions = ["Forward", "Midfielder", "Defender"]
+        topPlayers = pd.DataFrame()
+        for aux in range(1,4):
+            topPositionPlayers = self._get_top_players_by_postition(positions[aux - 1])
+            topPlayers = topPlayers.append(topPositionPlayers)
+
+        # Fill remaining spots
+        topPlayers.sort_values(by = ["event_points", "cost"], ascending = False, inplace = True)
+        topPlayers.reset_index(drop = True, inplace = True)
+        postion_limit = {"Defender": 5, "Midfielder": 5, "Forward": 3}
+        for _, player in topPlayers.iterrows():
+            if (dreamteam.team == player.team).sum() >= 3 or (dreamteam.position == player.position).sum() >= postion_limit[player.position]:
+                continue
+            dreamteam = dreamteam.append(player)
+            if self._validate_team(dreamteam):
+                break
+
+        # Add column to main DataFrame
+        self.data["gw_dreamteam"] = self.data.code.isin(dreamteam.code)
+
+    def save(self, gameweek):
+        self.data.to_csv("data/{}_{}.csv".format(self._data_type, self.gameweek), index = False, sep = ";")
